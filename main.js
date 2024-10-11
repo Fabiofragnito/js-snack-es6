09e5d49b43d9666eccb849c53dd1ed6660cbe255
Fabio Fragnito <fragnitofabio@gmail.com>
2024-10-11 14:57:11 +0200

    creato array obj

---
+++ main.js
@@ -0,0 +1,22 @@
+// Creare un array di oggetti:
+// Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+// Stampare a schermo la bici con peso minore.
+
+const bikes = [
+    { nome : 'bici1',
+      peso : 8  
+
+    },
+    { nome : 'bici2',
+      peso : 12 
+
+    },
+    { nome : 'bici3',
+      peso : 9 
+
+    },
+    { nome : 'bici4',
+      peso : 6  
+
+    },
+]
